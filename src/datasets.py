New datasets, normalization, fixed vizualization

src/datasets.py
@@ -1,17 +1,19 @@
-import os
-import glob
-from functools import partial, reduce
-
+# import os
+# import glob
+from functools import reduce
 
 import cv2
-import torch
+import h5py
+# import torch
+import numpy as np
 import torchvision
-from torch.utils.data import Dataset, random_split
+from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 
-from src.augmentations import get_aug
+# from src.augmentations import get_aug
 from src.utils import walk_files
+
 
 class MNIST(torchvision.datasets.MNIST):
     def __getitem__(self, index):
@@ -24,7 +26,34 @@ class MNIST(torchvision.datasets.MNIST):
                 and input is transformed original image
         """
         img = self.data[index]
-        # Add channels dimension and stack 3 identical tensors 
+        # Add channels dimension and stack 3 identical tensors
+        # Shape (H, W, channels)
+        img = img.unsqueeze(2).repeat(1, 1, 3)
+
+        # Convert to Numpy, so Albumentations can work
+        img = img.numpy()
+
+        if self.transform is not None:
+            augmented = self.transform(image=img, mask=img)
+            input, target = augmented["image"], augmented["mask"]
+        else:
+            input, target = img, img
+
+        return input, target
+
+
+class FashionMNIST(torchvision.datasets.FashionMNIST):
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (input, target) where target is untrasformed original image
+                and input is transformed original image
+        """
+        img = self.data[index]
+        # Add channels dimension and stack 3 identical tensors
         # Shape (H, W, channels)
         img = img.unsqueeze(2).repeat(1, 1, 3)
 
@@ -51,7 +80,7 @@ class CIFAR10(torchvision.datasets.CIFAR10):
                 and input is transformed original image
         """
         img = self.data[index]
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         if self.transform is not None:
             augmented = self.transform(image=img, mask=img)
@@ -72,7 +101,7 @@ class CIFAR100(torchvision.datasets.CIFAR100):
             tuple: (image, target) where target is untrasformed original image.
         """
         img = self.data[index]
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         if self.transform is not None:
             augmented = self.transform(image=img, mask=img)
@@ -92,14 +121,14 @@ class Set5(Dataset):
     """
     def __init__(
         self, root="datasets/Set5", train=False, transform=None):
-        assert train == False, "This dataset can be used only for validation"
+        assert train is False, "This dataset can be used only for validation"
         walker = walk_files(
             root, suffix=".png", prefix=True, remove_suffix=False
         )
-        
+
         self.files = list(walker)
         self.transform = transform
-        
+
     def __getitem__(self, index):
         """
         Args:
@@ -112,7 +141,7 @@ class Set5(Dataset):
         img = cv2.imread(self.files[index], cv2.IMREAD_UNCHANGED)
         # Covert to RGB and clip to [0., 1.]
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
+
         if self.transform is not None:
             augmented = self.transform(image=img, mask=img)
             input, target = augmented["image"], augmented["mask"]
@@ -142,9 +171,9 @@ class Urban100(Set5):
         transform (callable) – A function/transform that takes in the input and transforms it.
     """
     def __init__(
-        self, root="datasets/Urban100", train=False, transform=None
-        ):
+        self, root="datasets/Urban100", train=False, transform=None):
         super().__init__(root, train, transform)
+
 
 class Manga109(Set5):
     """
@@ -154,9 +183,9 @@ class Manga109(Set5):
         transform (callable) – A function/transform that takes in the input and transforms it.
     """
     def __init__(
-        self, root="datasets/Manga109", train=False, transform=None
-        ):
+        self, root="datasets/Manga109", train=False, transform=None):
         super().__init__(root, train, transform)
+
 
 class COIL100(Set5):
     """
@@ -166,8 +195,7 @@ class COIL100(Set5):
         transform (callable) – A function/transform that takes in the input and transforms it.
     """
     def __init__(
-        self, root="datasets/coil-100", train=False, transform=None
-        ):
+        self, root="datasets/coil-100", train=False, transform=None):
         super().__init__(root, train, transform)
 
 
@@ -180,14 +208,14 @@ class DIV2K(Dataset):
     """
     def __init__(self, root="datasets/", train=True, transform=None):
 
-        root += "DIV2K_"+ ('train' if train else 'valid') + "_LR_bicubic/X2"
+        root += "DIV2K_" + ('train' if train else 'valid') + "_LR_bicubic/X2"
         walker = walk_files(
             root, suffix=".png", prefix=True, remove_suffix=False
         )
-        
+
         self.files = list(walker)
         self.transform = transform
-        
+
     def __getitem__(self, index):
         """
         Args:
@@ -200,7 +228,7 @@ class DIV2K(Dataset):
         img = cv2.imread(self.files[index], cv2.IMREAD_UNCHANGED)
         # Covert to RGB and clip to [0., 1.]
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
+
         if self.transform is not None:
             augmented = self.transform(image=img, mask=img)
             input, target = augmented["image"], augmented["mask"]
@@ -211,6 +239,7 @@ class DIV2K(Dataset):
 
     def __len__(self):
         return len(self.files)
+
 
 class BSDS100(Set5):
     """
@@ -234,15 +263,15 @@ class TinyImageNet(Dataset):
     """
     def __init__(
         self, root="datasets/tiny-imagenet-200", train=True, transform=None):
-        
+
         root += "/train" if train else "/val"
         walker = walk_files(
             root, suffix=".JPEG", prefix=True, remove_suffix=False
         )
-        
+
         self.files = list(walker)
         self.transform = transform
-        
+
     def __getitem__(self, index):
         """
         Args:
@@ -263,9 +292,49 @@ class TinyImageNet(Dataset):
             input, target = img, img
 
         return input, target
-        
+
     def __len__(self):
         return len(self.files)
+
+
+class MedicalDecathlon(Dataset):
+    """Used to access images from MedicalDecathlon challenge
+    """
+    def __init__(
+        self, data_path="datasets/decathlon", filename="colon.h5", train=True, transform=None):
+        """
+        Args:
+            data_path (str): Path to folder with hp5 datasets
+            filename (str): {`colon`} type of data
+            train (bool): Flag to return train or val dataset
+        """
+
+        # Read all the data into memory
+        with h5py.File(data_path + "/" + filename, "r") as f:
+            if train:
+                # Take each 10th image so that they are not highly correlated
+                self.data = f['imgs_train'][::10]
+            else:
+                # Combine test and validation datasets
+                data_testing = f['imgs_testing'][::10]
+                data_validation = f['imgs_validation'][::10]
+                self.data = np.concatenate((data_validation, data_testing))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img = self.data[idx]
+        # Convert (H, W) -> (H, W, 3) for transform
+        img = img.repeat(3, axis=2)
+        if self.transform is not None:
+            augmented = self.transform(image=img, mask=img)
+            input, target = augmented["image"], augmented["mask"]
+        else:
+            input, target = img, img
+
+        return input, target
 
 
 def get_dataloader(
@@ -293,6 +362,10 @@ def get_dataloader(
         dataset = MNIST("datasets/", train, transform)
         all_datasets.append(dataset)
 
+    if "fashion_mnist" in datasets:
+        dataset = FashionMNIST("datasets/", train, transform)
+        all_datasets.append(dataset)
+    
     if "cifar10" in datasets:
         dataset = CIFAR10("datasets/", train, transform)
         all_datasets.append(dataset)

src/experiment.py
@@ -1,4 +1,4 @@
-import os
+# import os
 import math
 from collections import OrderedDict
 
@@ -7,14 +7,13 @@ import torch
 import torchvision
 import pytorch_lightning as pl
 import torch.nn.functional as F
-import photosynthesis_metrics as pm
+# import photosynthesis_metrics as pm
 
 
 from src.augmentations import get_aug
 from src.datasets import get_dataloader
 from src.modules import Identity, MODEL_FROM_NAME
 from src.utils import METRIC_FROM_NAME
-
 
 
 class BaseModel(pl.LightningModule):
@@ -63,6 +62,8 @@ class BaseModel(pl.LightningModule):
 
         loss = self.criterion(prediction, target)
 
+        try:
+            print(self.current_lr)
         tqdm_dict = {'train_loss': loss}
         output = OrderedDict({
             'progress_bar': tqdm_dict,
@@ -101,6 +102,8 @@ class BaseModel(pl.LightningModule):
 
         tqdm_dict = {}
         log_dict = {}
+        # Same as log_dict but showed all at one plot
+        model_dict = {}
 
         # Reduce per-image metrics
         self.metric_names.append("loss")
@@ -113,7 +116,10 @@ class BaseModel(pl.LightningModule):
             if len(outputs) == 0:
                 print(outputs)
             log_dict["validation/" + metric_name] = metric_total / len(outputs)
-        self.metric_names.pop() # Remove `loss` from this list
+            model_dict[metric_name] = log_dict["validation/" + metric_name]
+
+        self.metric_names.pop()  # Remove `loss` from this list
+        model_dict.pop("loss")
 
         # Collect computed image features into a vector of size (val_size, feat_size)
         all_input_features = [out["input_features"] for out in outputs]
@@ -127,6 +133,7 @@ class BaseModel(pl.LightningModule):
         for i, name in enumerate(self.feat_metric_names):
             score = self.feat_metrics[i](input_features.cpu(), self.validation_features.cpu())
             log_dict["validation/" + name] = torch.tensor(score)
+            model_dict[metric_name] = log_dict["validation/" + metric_name]
 
         if "msid" in self.feat_metric_names:
             score = []
@@ -138,9 +145,11 @@ class BaseModel(pl.LightningModule):
                     )
                 )
             log_dict["validation/msid"] = torch.mean(torch.tensor(score))
+            model_dict["msid"] = log_dict["validation/msid"]
 
         tqdm_dict["val_loss"] = log_dict["validation/loss"]
 
+        self.logger.experiment.add_scalars(f"models/{self.hp.name}", model_dict, self.current_epoche)
         result = {'progress_bar': tqdm_dict, 'log': log_dict, 'val_loss': log_dict["validation/loss"]}
         return result
 
@@ -149,17 +158,21 @@ class BaseModel(pl.LightningModule):
         optimizer = torch.optim.Adam(
             self.parameters(),
             lr=self.hparams.lr,
-            weight_decay=self.hparams.weight_decay,   
+            weight_decay=self.hparams.weight_decay,
+            amsgrad=True
         )
 
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
         return [optimizer], [scheduler]
 
     def train_dataloader(self):
         # Transforms
         hp = self.hparams
         transform = get_aug(
-            hp.aug_type, hp.task, hp.data_mean, hp.data_std, hp.size
+            aug_type=hp.aug_type,
+            task=hp.task,
+            dataset=hp.datasets[0],
+            size=hp.size
         )
 
         # Normalize and convert to tensor
@@ -178,10 +191,13 @@ class BaseModel(pl.LightningModule):
 
         # Transforms
         transform = get_aug(
-            hp.aug_type, hp.task, hp.data_mean, hp.data_std, hp.size
+            aug_type=hp.aug_type,
+            task=hp.task,
+            dataset=hp.datasets[0],
+            size=hp.size
         )
 
-        # # Normalize and convert to tensor
+        # Normalize and convert to tensor
 
         val_loader = get_dataloader(
             datasets=hp.datasets,
@@ -198,17 +214,14 @@ class BaseModel(pl.LightningModule):
 
         # Upscale images by bilinear interpolation to get the same image size. 
         if self.hparams.task == "sr":
-            images = F.interpolate(images, target.shape[:-2],  mode="bilinear")
+            images = F.interpolate(images, size=target.shape[:-2], mode="bilinear")
+
         N = self.hparams.num_images_to_log
-        grid_input = torchvision.utils.make_grid(images[:N], nrow=int(math.sqrt(N)))
-        grid_target = torchvision.utils.make_grid(target[:N], nrow=int(math.sqrt(N)))
-        grid_output = torchvision.utils.make_grid(output[:N], nrow=int(math.sqrt(N)))
+        grid_input = torchvision.utils.make_grid(images[:N], nrow=int(math.sqrt(N)), normalize=True)
+        grid_target = torchvision.utils.make_grid(target[:N], nrow=int(math.sqrt(N)), normalize=True)
+        grid_output = torchvision.utils.make_grid(output[:N], nrow=int(math.sqrt(N)), normalize=True)
 
         # Concat along X axis
         final_image = torch.cat([grid_input, grid_output, grid_target], dim=2)
-        # Change channes 
-        # if final_image.shape[0] == 3:
-            # final_image = 
-
         self.logger.experiment.add_image(f'Validation_images', final_image, self.current_epoch)
 